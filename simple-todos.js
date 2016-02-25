152474d5a49a850b43aff2b7fe8bb2bd3854b45a
Joseph Nguyen <vietjew@gmail.com>
2016-02-25 15:46:19 -0800

    Create android app. Add functionality for hiding completed items.

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,14 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
     }
   });
 
@@ -24,6 +31,9 @@ if (Meteor.isClient) {
 
       // Reset form
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
