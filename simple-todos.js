5deeb4cb63aaafb0b64bd4e1c9f390f6e18ee914
Joseph Nguyen <vietjew@gmail.com>
2016-02-26 10:13:27 -0800

    Add account ui packages, update so that only logged in users can add tasks. Tasks show user who submitted them.

--- simple-todos.js
+++ simple-todos.js
@@ -29,10 +29,12 @@ if (Meteor.isClient) {
       // Insert a task into the collection
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(), // current time
+        owner: Meteor.userId(), // _id of logged in user
+        username: Meteor.user().username // username of logged in user
       });
 
-      // Reset form
+      // Clear form
       event.target.text.value = "";
     },
     "change .hide-completed input": function (event) {
@@ -50,6 +52,10 @@ if (Meteor.isClient) {
     "click .delete": function () {
       Tasks.remove(this._id);
     }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
